365ed71cf4722b225e456082430b67ff0e9b4ebc
pukaradhikari1 <pukar.adhikari656@gmail.com>
2026-02-12 21:50:01 +0545

    completed

--- Backend/models/predict_resnet.py
+++ Backend/models/predict_resnet.py
@@ -6,22 +6,22 @@ import json
 MODEL_PATH = r"C:\Users\Hp Victus\Desktop\project 4\Backend\saved_model\plant_disease_resnet.keras"
 IMAGE_PATH = r"C:\Users\Hp Victus\Desktop\project 4\Backend\test_leaf.jpg"
 
-# load class names 
+
 with open("class_names.json", "r") as f:
     class_names = json.load(f)
 
-# load the model
+
 model = tf.keras.models.load_model(MODEL_PATH)
 print(" Model loaded successfully.")
 
-# load and prep the image
+
 img = Image.open(IMAGE_PATH).convert("RGB")
 img = img.resize((224, 224))
 
-# convert to array
+
 img_array = np.expand_dims(np.array(img), axis=0)
 
-# make prediction
+
 predictions = model.predict(img_array)
 
 predicted_index = np.argmax(predictions[0])
